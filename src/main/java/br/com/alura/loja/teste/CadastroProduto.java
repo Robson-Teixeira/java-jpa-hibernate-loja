4f077fc492c64c91190f6fac9516d9ff2a3a628a
Robson-Teixeira <robsonmteixeira@outlook.com>
2023-12-17 10:59:50 -0300

    Mapeando entidades: Mais mapeamentos

--- src/main/java/br/com/alura/loja/teste/CadastroProduto.java
+++ src/main/java/br/com/alura/loja/teste/CadastroProduto.java
@@ -7,6 +7,7 @@ import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
 import br.com.alura.loja.dao.ProdutoDao;
+import br.com.alura.loja.modelo.Categoria;
 import br.com.alura.loja.modelo.Produto;
 import br.com.alura.loja.util.JPAUtil;
 
@@ -14,13 +15,12 @@ public class CadastroProduto {
 
 	public static void main(String[] args) {
 
-		Produto celular = new Produto();
-		celular.setNome("Xiaomi Redmi 10");
-		celular.setDescricao("Smartphone Android de 6.5 polegadas e de 2400x1080 pixels. "
-				+ "Conectividade lte 4G, Wi-Fi e gps, leitor multimídia, rádio, "
-				+ "videoconferência e Bluetooth. Memória interna de 128 gb, "
-				+ "câmera traseira de 50 megapixels. Com apenas 8.9 milímetros de espessura.");
-		celular.setPreco(new BigDecimal("800"));
+		Produto celular = new Produto("Xiaomi Redmi 10",
+				"Smartphone Android de 6.5 polegadas e de 2400x1080 pixels. "
+						+ "Conectividade lte 4G, Wi-Fi e gps, leitor multimídia, rádio, "
+						+ "videoconferência e Bluetooth. Memória interna de 128 gb, "
+						+ "câmera traseira de 50 megapixels. Com apenas 8.9 milímetros de espessura.",
+						new BigDecimal("800"), Categoria.CELULARES);
 
 		EntityManager entityManager = JPAUtil.getEntityManager();
 
